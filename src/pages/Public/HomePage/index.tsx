b8055b5901f446266dce0363a33437219d29af03
Huỳnh Minh Phước <118465492+PhuocHuynh63@users.noreply.github.com>
2025-09-07 13:28:13 +0700

    Update package dependencies, add HomePage component, and remove GenerateUUID utility function.

---
+++ src/pages/Public/HomePage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePageClient = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default HomePageClient
---
+++ src/types/IUtils/index.d.ts
@@ -0,0 +1,7 @@
+declare namespace UTILS {
+    export interface INavLink {
+        className?: string;
+        href: string;
+        children: React.ReactNode;
+    }
+}
