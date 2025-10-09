d08d2de5976c2ccee53893197bd99bf065f27c12
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2025-10-09 16:35:06 +0700

    Add DFVN Graphit font and update DetailInfo component styles
    - Introduced a new font variable for DFVN Graphit in globals.css.
    - Updated layout.tsx to include DFVN Graphit font in the body class.
    - Refined DetailInfo component styles for gender selection and birth year input, enhancing user interaction and accessibility.
    - Removed unused BDStreetSignSans font file.
    - Added utility to generate a list of years for birth year selection in Date utility.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,19 +9,14 @@ import VietnameseLoading from "@components/Molecules/Loading";
 import VietnameseHistoryLoading from "@components/Molecules/HistoryLoading";
 import localFont from 'next/font/local';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const StreetSignSans = localFont({
   src: './fonts/StreetSignSans.otf',
   variable: '--font-bd-street-sign',
+});
+
+const DFVNGraphit = localFont({
+  src: './fonts/DFVNGraphitRegular.otf',
+  variable: '--font-dfvn-graphit',
 });
 
 export const metadata: Metadata = {
@@ -40,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${StreetSignSans.variable} antialiased`}
+        className={`${StreetSignSans.variable} ${DFVNGraphit.variable} antialiased`}
       >
         <ToastContainer />
         <QueryProviderWrapper>
--- src/pages/Public/HomePage/DetailInfo/index.tsx
+++ src/pages/Public/HomePage/DetailInfo/index.tsx
@@ -3,66 +3,55 @@ import React from 'react'
 import ScrollPaper from "../../../../../public/ScrollPaper.svg"
 import frame from "../../../../../public/frame.svg"
 import ButtonImage from "../../../../../public/Button.svg"
+import { years } from '@utils/Date'
 
 const DetailInfo = () => {
+
     return (
         <div className='w-full flex items-center justify-center'>
             <div className='relative w-full max-w-5xl mx-auto'>
                 <Image src={ScrollPaper} alt="Scroll Paper" width={1000} height={1000} className="w-full h-auto" />
 
                 <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[95%] md:w-[80%] flex items-center justify-center flex-col">
-
-                    <h1 className='text-center text-2xl sm:text-4xl lg:text-6xl font-bd-street-sign text-secondary mb-2 md:mb-4'>
+                    <h1 className='text-center text-2xl sm:text-4xl lg:text-6xl font-bd-street-sign text-secondary mb-2'>
                         THÔNG TIN CHI TIẾT
                     </h1>
 
                     <div className='relative w-full flex justify-center'>
                         <Image src={frame} alt="Frame" width={600} height={800} className="w-full h-auto max-w-[600px]" />
 
-                        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[60%] md:w-[45%] flex items-center justify-start flex-col space-y-2 sm:space-y-4 md:space-y-6'>
+                        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[60%] md:w-[45%] flex items-center justify-start flex-col space-y-2 sm:space-y-4'>
+                            {/* --- Hàng Giới tính --- */}
                             <div className='w-full flex items-center justify-start gap-2'>
                                 <p className='text-center text-xl md:text-3xl lg:text-4xl font-bd-street-sign text-third'>
                                     GIỚI TÍNH
                                 </p>
-                                <div className='flex items-center justify-center gap-x-6 text-base md:text-xl ml-7'>
-                                    <div className='flex items-center'>
-                                        <label className='flex items-center cursor-pointer'>
-                                            <input type="radio" name="gender" id="male" className='appearance-none peer' />
-                                            <span className='w-5 h-5 border-2 border-gray-400 rounded-full flex items-center justify-center transition peer-checked:border-primary'>
-                                                <span className='w-2.5 h-2.5 bg-primary rounded-full scale-0 transition-transform peer-checked:scale-100'></span>
-                                            </span>
-                                            <span className='ml-2'>Nam</span>
-                                        </label>
-                                    </div>
-                                    <div className='flex items-center'>
-                                        <label className='flex items-center cursor-pointer'>
-                                            <input type="radio" name="gender" id="female" className='appearance-none peer' />
-                                            <span className='w-5 h-5 border-2 border-gray-400 rounded-full flex items-center justify-center transition peer-checked:border-primary'>
-                                                <span className='w-2.5 h-2.5 bg-primary rounded-full scale-0 transition-transform peer-checked:scale-100'></span>
-                                            </span>
-                                            <span className='ml-2'>Nữ</span>
-                                        </label>
-                                    </div>
+                                <div className='flex items-center justify-center gap-x-4 ml-7 text-sm md:text-xl md:justify-self-start'>
+                                    <label className='flex items-center cursor-pointer'>
+                                        <input type="radio" name="gender" id="male" className='appearance-none peer' />
+                                        <span className='w-4 h-4 border-2 border-gray-700 rounded-full p-0.5 transition peer-checked:border-primary peer-checked:bg-primary bg-clip-content'></span>
+                                        <span className='ml-2 font-dfvn-graphit text-third'>Nam</span>
+                                    </label>
+                                    <label className='flex items-center cursor-pointer'>
+                                        <input type="radio" name="gender" id="female" className='appearance-none peer' />
+                                        <span className='w-4 h-4 border-2 border-gray-700 rounded-full p-0.5 transition peer-checked:border-primary peer-checked:bg-primary bg-clip-content'></span>
+                                        <span className='ml-2 font-dfvn-graphit text-third'>Nữ</span>
+                                    </label>
                                 </div>
                             </div>
 
+                            {/* --- Hàng Năm sinh --- */}
                             <div className='w-full flex items-center justify-start gap-2'>
                                 <p className='text-center text-xl md:text-3xl lg:text-4xl font-bd-street-sign text-third'>
                                     NĂM SINH
                                 </p>
-                                <div className='flex items-center justify-center hover:opacity-95 ml-3'>
-                                    <div className='relative flex items-center justify-center gap-x-2'>
-                                        <Image src={ButtonImage} alt='Button' width={110} height={110} className='cursor-pointer w-[80px] h-auto sm:w-[90px] lg:w-[110px]' />
-                                        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer'>
-                                            <select className='text-center text-base md:text-xl lg:text-2xl font-bd-street-sign text-third bg-transparent border-none outline-none cursor-pointer'>
-                                                <option value="1990">1990</option>
-                                                <option value="1991">1991</option>
-                                                <option value="1992">1992</option>
-                                                <option value="1993">1993</option>
-                                                <option value="1994">1994</option>
-                                            </select>
-                                        </div>
-                                    </div>
+                                <div className='relative flex items-center justify-center hover:opacity-95 ml-3 md:justify-self-start'>
+                                    <Image src={ButtonImage} alt='Button' width={110} height={110} className='cursor-pointer w-[100px] h-auto sm:w-[130px]' />
+                                    <select className='absolute left-1/2 -translate-x-1/2 w-[80%] h-full text-center text-sm font-dfvn-graphit text-third bg-transparent border-none outline-none cursor-pointer'>
+                                        {years.map((year) => (
+                                            <option key={year} value={year} className="bg-secondary text-primary">{year}</option>
+                                        ))}
+                                    </select>
                                 </div>
                             </div>
 
