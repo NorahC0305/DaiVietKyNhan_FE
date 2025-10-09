7d66a755e2cf84f6bbc8bc001af60568222bfebd
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2025-10-09 18:03:48 +0700

    Update CountDown component styles for improved responsiveness and visual hierarchy
    - Adjusted text sizes for headings and labels to enhance readability.
    - Modified image dimensions for better layout on various screen sizes.
    - Improved spacing and alignment for a more cohesive user experience.

--- src/pages/Public/HomePage/CountDown/index.tsx
+++ src/pages/Public/HomePage/CountDown/index.tsx
@@ -2,9 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 import ScrollPaper from "../../../../../public/ScrollPaper.svg"
 import frame from "../../../../../public/frame.svg"
-import ButtonImage from "../../../../../public/Button.svg"
 import ButtonHeight from "../../../../../public/ButtonHeight.svg"
-import { years } from '@utils/Date'
 
 const DetailInfo = () => {
 
@@ -15,20 +13,18 @@ const DetailInfo = () => {
 
                 <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[95%] md:w-[80%] flex items-center justify-center flex-col">
                     <div className='flex items-center justify-center flex-col mt-2'>
-                        <h1 className='text-center text-6xl font-bd-street-sign text-secondary mb-2'>
+                        <h1 className='text-center text-5xl md:text-6xl  font-bd-street-sign text-secondary md:mb-2 mb-1'>
                             ĐẠI VIỆT KỲ NHÂN
                         </h1>
 
-                        <h2 className='text-center text-4xl font-bd-street-sign text-third mb-2'>
+                        <h2 className='text-center md:text-4xl text-3xl font-bd-street-sign text-third md:mb-2'>
                             Trang web chính thức ra mắt
                         </h2>
 
-                        <h2 className='text-center text-4xl font-bd-street-sign text-third mb-2'>
+                        <h2 className='text-center md:text-4xl text-2xl font-bd-street-sign text-third md:mb-2'>
                             20/10/2025
                         </h2>
                     </div>
-
-
 
                     <div className='relative w-full flex justify-center'>
                         <Image src={frame} alt="Frame" className="w-full h-auto max-w-[550px] md:max-w-[800px]" />
@@ -36,7 +32,7 @@ const DetailInfo = () => {
                         <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[100%] md:w-[90%] flex items-center justify-start flex-col'>
                             {/* --- Title Count Down --- */}
                             <div className='w-full flex items-center justify-center gap-2'>
-                                <h3 className='text-center text-2xl mb-1 font-semibold font-dfvn-graphit text-secondary'>
+                                <h3 className='text-center md:text-2xl text-lg md:mb-1 font-semibold font-dfvn-graphit text-secondary'>
                                     Thời gian còn lại
                                 </h3>
                             </div>
@@ -44,10 +40,10 @@ const DetailInfo = () => {
                             {/* --- Count Down --- */}
                             <div className='flex items-center justify-center '>
                                 {/* --- Day --- */}
-                                <div className='relative  flex items-center justify-center '>
-                                    <Image src={ButtonHeight} alt="button" className="h-full max-w-[300px] md:max-w-[138px]" />
+                                <div className='relative flex items-center justify-center '>
+                                    <Image src={ButtonHeight} alt="button" className="h-full max-w-[100px] md:max-w-[138px]" />
                                     <div className='absolute w-full h-full flex items-center justify-start flex-col'>
-                                        <span className='text-center font-dfvn-graphit h-full text-2xl font-bold mt-2 text-secondary'>
+                                        <span className='text-center font-dfvn-graphit h-full md:text-2xl text-lg font-bold text-secondary'>
                                             6
                                         </span>
                                         <span className='text-center font-dfvn-graphit h-full text-md text-third'>
@@ -58,9 +54,9 @@ const DetailInfo = () => {
 
                                 {/* --- Month --- */}
                                 <div className='relative  flex items-center justify-center gap-2'>
-                                    <Image src={ButtonHeight} alt="button" className="h-auto max-w-[130px] md:max-w-[138px]" />
+                                    <Image src={ButtonHeight} alt="button" className="h-auto max-w-[100px] md:max-w-[138px]" />
                                     <div className='absolute w-full h-full flex items-center justify-start flex-col'>
-                                        <span className='text-center font-dfvn-graphit h-full text-2xl font-bold mt-2 text-secondary'>
+                                        <span className='text-center font-dfvn-graphit h-full md:text-2xl text-xl font-bold text-secondary'>
                                             23
                                         </span>
                                         <span className='text-center font-dfvn-graphit h-full text-md text-third'>
@@ -71,9 +67,9 @@ const DetailInfo = () => {
 
                                 {/* --- Year --- */}
                                 <div className='relative  flex items-center justify-center gap-2'>
-                                    <Image src={ButtonHeight} alt="button" className="h-auto max-w-[130px] md:max-w-[138px]" />
+                                    <Image src={ButtonHeight} alt="button" className="h-auto max-w-[100px] md:max-w-[138px]" />
                                     <div className='absolute w-full h-full flex items-center justify-start flex-col'>
-                                        <span className='text-center font-dfvn-graphit h-full text-2xl font-bold mt-2 text-secondary'>
+                                        <span className='text-center font-dfvn-graphit h-full md:text-2xl text-xl font-bold text-secondary'>
                                             59
                                         </span>
                                         <span className='text-center font-dfvn-graphit h-full text-md text-third'>
@@ -84,9 +80,9 @@ const DetailInfo = () => {
 
                                 {/* --- Hour --- */}
                                 <div className='relative  flex items-center justify-center gap-2'>
-                                    <Image src={ButtonHeight} alt="button" className="h-auto max-w-[130px] md:max-w-[138px]" />
+                                    <Image src={ButtonHeight} alt="button" className="h-auto max-w-[100px] md:max-w-[138px]" />
                                     <div className='absolute w-full h-full flex items-center justify-start flex-col'>
-                                        <span className='text-center font-dfvn-graphit h-full text-2xl font-bold mt-2 text-secondary'>
+                                        <span className='text-center font-dfvn-graphit h-full md:text-2xl text-xl font-bold text-secondary'>
                                             59
                                         </span>
                                         <span className='text-center font-dfvn-graphit h-full text-md text-third'>
@@ -96,21 +92,21 @@ const DetailInfo = () => {
                                 </div>
                             </div>
 
-                            <div className='w-full flex items-center justify-center gap-2 mt-5'>
+                            <div className='w-full flex items-center justify-center gap-2 md:mt-5 mt-2'>
                                 <div className='flex  flex-col items-end justify-center'>
-                                    <p className='text-end text-xl font-semibold font-dfvn-graphit text-secondary'>
+                                    <p className='text-end md:text-xl text-md font-semibold font-dfvn-graphit text-secondary'>
                                         Số người đăng ký trước:
                                     </p>
-                                    <p className='text-end text-xl font-semibold font-dfvn-graphit text-secondary'>
+                                    <p className='text-end md:text-xl text-md font-semibold font-dfvn-graphit text-secondary'>
                                         Số xu người đăng ký trước nhận được:
                                     </p>
                                 </div>
 
                                 <div className='relative flex items-center justify-center gap-2'>
-                                    <Image src={ButtonHeight} alt="button" className="h-full max-w-[300px] md:max-w-[110px]" />
+                                    <Image src={ButtonHeight} alt="button" className="h-full max-w-[80px] md:max-w-[110px]" />
 
                                     <div className='absolute flex items-center justify-start flex-col'>
-                                        <p className='flex items-center justify-center text-center text-secondary font-dfvn-graphit h-fit text-2xl mt-1'>
+                                        <p className='flex items-center justify-center text-center text-secondary font-dfvn-graphit h-fit text-xl mt-1'>
                                             099
                                         </p>
                                     </div>
