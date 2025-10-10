51602f313c2716705afb228aa4b599c6e58070e6
Huỳnh Minh Phước <118465492+PhuocHuynh63@users.noreply.github.com>
2025-10-10 14:42:39 +0700

    Refactor Home component to improve user data handling
    - Updated userMe function to return a more structured response using IBackendResponse.
    - Adjusted HomePageClient to correctly utilize the user data from the updated response format.
    - Ensured UserSchema is imported for type safety in user data management.

--- src/models/user/entity/index.ts
+++ src/models/user/entity/index.ts
@@ -2,21 +2,21 @@ import { USER } from "@constants/user";
 import z from "zod";
 
 export const UserSchema = z.object({
-    id: z.number(),
-    email: z.string().email(),
-    name: z.string(),
-    password: z.string().min(6).max(100),
-    phoneNumber: z.string().min(9).max(15),
-    gender: z.enum([USER.GENDER.MALE, USER.GENDER.FEMALE, USER.GENDER.OTHER]).nullable(),
-    birthDate: z.coerce.date().nullable(),
-    avatar: z.string().nullable(),
-    status: z.enum([USER.USER_STATUS.ACTIVE, USER.USER_STATUS.INACTIVE]),
-    roleId: z.number().positive(),
-    createdById: z.number().nullable(),
-    updatedById: z.number().nullable(),
-    deletedById: z.number().nullable(),
-    deletedAt: z.date().nullable(),
-    createdAt: z.date(),
-    updatedAt: z.date()
-  })
+  id: z.number(),
+  email: z.string().email(),
+  name: z.string(),
+  password: z.string().min(6).max(100),
+  phoneNumber: z.string().min(9).max(15),
+  gender: z.enum([USER.GENDER.MALE, USER.GENDER.FEMALE, USER.GENDER.OTHER]).nullable(),
+  birthDate: z.coerce.date().nullable(),
+  avatar: z.string().nullable(),
+  status: z.enum([USER.USER_STATUS.ACTIVE, USER.USER_STATUS.INACTIVE]),
+  roleId: z.number().positive(),
+  createdById: z.number().nullable(),
+  updatedById: z.number().nullable(),
+  deletedById: z.number().nullable(),
+  deletedAt: z.date().nullable(),
+  createdAt: z.date(),
+  updatedAt: z.date()
+})
 export type IUser = z.infer<typeof UserSchema>
