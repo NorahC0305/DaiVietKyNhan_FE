0fbbfa7ebfd8a4b361335cf603ad46e07abbd026
Huỳnh Minh Phước <118465492+PhuocHuynh63@users.noreply.github.com>
2025-10-19 21:22:32 +0700

    Add Guide modal to GameFrame component
    - Introduced a new Guide modal to the GameFrame component for enhanced user assistance.
    - Implemented state management for the Guide modal, allowing users to toggle its visibility.
    - Updated the layout to include a button for opening the Guide modal, improving accessibility.
    - Refactored land configuration to a centralized constant for better maintainability and clarity.

--- src/constants/common/index.ts
+++ src/constants/common/index.ts
@@ -2,15 +2,26 @@ const COOKIES = {
     ACCESS_TOKEN: 'accessToken',
 };
 
-const ROLE = {
-    ADMIN: {
-        ID: 1,
-        NAME: 'admin',
+const LAND_CONFIG = {
+    'Sơn Tinh': {
+        img: 'https://res.cloudinary.com/dznt9yias/image/upload/v1760882410/god-profiles/images/file_hubadj.png',
+        tenNui: 'Núi Tản Viên',
+        color: '#41821E'
     },
-    CUSTOMER: {
-        ID: 2,
-        NAME: 'customer',
+    'Thánh Gióng': {
+        img: 'https://res.cloudinary.com/dznt9yias/image/upload/v1760882465/god-profiles/images/file_umblfc.png',
+        tenNui: 'Đền Phù Đổng',
+        color: '#EF493D'
     },
-};
-
-export { COOKIES, ROLE };
+    'Chử Đồng Tử': {
+        img: 'https://res.cloudinary.com/dznt9yias/image/upload/v1760882435/god-profiles/images/file_rogijq.png',
+        tenNui: 'Đầm Dạ Trạch',
+        color: '#2B638F'
+    },
+    'Liễu Hạnh': {
+        img: 'https://res.cloudinary.com/dznt9yias/image/upload/v1760882511/god-profiles/images/file_jqsu8f.png',
+        tenNui: 'Phủ Tây Hồ',
+        color: '#8D3BBB'
+    }
+}
+export { COOKIES, LAND_CONFIG };
