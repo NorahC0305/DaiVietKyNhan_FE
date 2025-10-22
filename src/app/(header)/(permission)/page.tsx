204517d6745d4eaa843c0613553ff055792c6c75
Adu Ang Du <vunguyen003pv@gmail.com>
2025-10-22 11:40:08 +0700

    Add dynamic export to multiple components for server-side rendering

--- src/app/(header)/(permission)/page.tsx
+++ src/app/(header)/(permission)/page.tsx
@@ -1,12 +1,20 @@
+export const dynamic = 'force-dynamic';
+
+
 import HomePageClient from "@pages/Public/HomePage";
 import userService from "@services/user";
 import { UserSchema } from "@models/user/entity";
 import { IBackendResponse } from "@models/backend";
 import { IUser } from "@models/user/entity";
 import systemService from "@services/system";
-import { GetSystemConfigWithAmountUserResSchema, IGetSystemConfigWithAmountUserResponse } from "@models/system/response";
+import {
+  GetSystemConfigWithAmountUserResSchema,
+  IGetSystemConfigWithAmountUserResponse,
+} from "@models/system/response";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@lib/authOptions";
+import { getAttendanceListSSR } from "@lib/attendance";
+import { AttendanceProvider } from "@contexts/AttendanceContext";
 
 async function userMe() {
   try {
@@ -18,14 +26,22 @@ async function userMe() {
 }
 
 export default async function Home() {
-  const session = await getServerSession(authOptions) as UTILS.ISession;
-  const user = await userMe() as IBackendResponse<typeof UserSchema>;
-  const activeWithAmountUser = await systemService.getActiveWithAmountUser(true) as IBackendResponse<typeof GetSystemConfigWithAmountUserResSchema>;
+  const session = (await getServerSession(authOptions)) as UTILS.ISession;
+  const user = (await userMe()) as IBackendResponse<typeof UserSchema>;
+  const activeWithAmountUser = (await systemService.getActiveWithAmountUser(
+    true
+  )) as IBackendResponse<typeof GetSystemConfigWithAmountUserResSchema>;
+  const initialAttendanceList = await getAttendanceListSSR();
 
   return (
-    <HomePageClient
-      user={user.data as IUser}
-      activeWithAmountUser={activeWithAmountUser.data as IGetSystemConfigWithAmountUserResponse}
-      accessToken={session?.accessToken} />
+    <AttendanceProvider initialAttendanceList={initialAttendanceList}>
+      <HomePageClient
+        user={user.data as IUser}
+        activeWithAmountUser={
+          activeWithAmountUser.data as IGetSystemConfigWithAmountUserResponse
+        }
+        accessToken={session?.accessToken}
+      />
+    </AttendanceProvider>
   );
 }
--- src/pages/Public/About-us/index.tsx
+++ src/pages/Public/About-us/index.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import React from "react";
 import Image from "next/image";
 import RadialGradial from "@components/Atoms/RadialGradient";
