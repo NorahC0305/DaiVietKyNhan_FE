ada96e4b7e2191730e68a2aeb9d5d151956a13cc
Adu Ang Du <vunguyen003pv@gmail.com>
2025-10-22 15:22:06 +0700

    Refactor text layout in Guide component for improved readability
    - Adjusted text structure to display two lines instead of one for better visual clarity.
    - Enhanced the description in the Guide component to provide a more coherent narrative flow.

--- src/components/Molecules/Popup/Guide/index.tsx
+++ src/components/Molecules/Popup/Guide/index.tsx
@@ -71,9 +71,9 @@ const Guide = ({ isOpen, onClose, user }: { isOpen: boolean, onClose: () => void
                                         <div className='relative w-[300px] lg:w-[400px] h-[80px] lg:h-[117px] flex items-center justify-center my-5'>
                                             <Image src="https://res.cloudinary.com/dznt9yias/image/upload/v1760726766/frame_hfc9ot.svg" alt="frame" fill />
                                             <div className='absolute inset-0 flex items-center justify-center '>
-                                                <div className='flex items-center justify-center w-[60%]'>
-                                                    <span className='text-center lg:text-2xl text-base font-bd-street-sign text-[#CE7346]'>Trang sử còn đợi ai đưa,
-                                                        Khai Nhân Mở Ấn, chuyện xưa hóa gần.</span>
+                                                <div className='flex  flex-col items-center justify-center w-[64%]'>
+                                                    <span className='text-center lg:text-2xl text-base font-bd-street-sign text-[#CE7346]'>Trang sử còn đợi ai đưa,</span>
+                                                    <span className='text-center lg:text-2xl text-base font-bd-street-sign text-[#CE7346]'>Khai Nhân Mở Ấn, chuyện xưa hóa gần.</span>
                                                 </div>
                                             </div>
                                         </div>
@@ -98,7 +98,7 @@ const Guide = ({ isOpen, onClose, user }: { isOpen: boolean, onClose: () => void
                                             <br />
                                             <br />
                                             <span className='lg:text-xl text-sm font-extrabold text-secondary text-justify leading-relaxed'>
-                                                đã xâm nhập vào Kỳ Giới, khiến linh khí suy yếu và các Kỳ Nhân bị phong ấn trong những cuộn giấy cổ, gọi là Kỳ Ấn.
+                                              Thế nhưng, một thế lực bí ẩn đã xâm nhập vào Kỳ Giới, khiến linh khí suy yếu và các Kỳ Nhân bị phong ấn trong những cuộn giấy cổ, gọi là Kỳ Ấn.
                                             </span>
                                         </div>
                                     </div>
